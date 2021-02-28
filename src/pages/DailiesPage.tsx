1991230f8be304ddc7ce24cc0e1ac549c6f14a07
Victor <ngo.victor20@gmail.com>
2021-02-27 23:47:39 -0500

    Added dispatch to dailies page

--- src/pages/DailiesPage.tsx
+++ src/pages/DailiesPage.tsx
@@ -4,6 +4,7 @@ import {useAppSelector, useAppDispatch} from 'redux/hooks';
 
 const DailiesPage = () => {
     const { dailyChecklist } = useAppSelector(state=>state.checklist);
+
     return (
         <div>
             <ChecklistRenderer checklist={dailyChecklist}/>
