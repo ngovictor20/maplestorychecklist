a7d7a1254ee519be8de1ae919cc4ada5e457a2cc
Victor <ngo.victor20@gmail.com>
2021-02-28 00:13:33 -0500

    Added selected effect

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -15,8 +15,8 @@ const CharacterCard: React.FC<CharacterProps> = ({ className, level, name, index
     const dispatch = useAppDispatch();
 
     return (
-        <div className="cursor-pointer border-blue-600 border rounded-lg flex items-center" onClick={() => dispatch(setCharIndex(index))}>
-            <img className=" w-12 h-12 m-2 rounded-full ring-1 ring-black" src={`/${Class[className]}.svg`} alt={`${Class[className]}`} />
+        <div className={`cursor-pointer border-blue-400 border rounded-lg flex items-center ${selected ? "text-white bg-blue-400" : ""}`} onClick={() => dispatch(setCharIndex(index))}>
+            <img className=" w-12 h-12 m-2 rounded-full ring-1 blue-500" src={`/${Class[className]}.svg`} alt={`${Class[className]}`} />
             <div className="pr-2">
                 <p>{name}</p>
                 <p>{level}</p>
