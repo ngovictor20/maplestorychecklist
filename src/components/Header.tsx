87439b86bf3a0659dc1664d12b1287aa95d37449
Victor <ngo.victor20@gmail.com>
2021-03-06 01:13:49 -0500

    Header nav buttons to right

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,27 +7,31 @@ import { DialogType } from './Dialog/types';
 
 const Header = () => {
     const dispatch = useAppDispatch();
-    const {renderDialog,toggleDialog} = useDialog();
+    const { renderDialog, toggleDialog } = useDialog();
     return (
-        <div className="sticky top-0 z-30 h-14 px-36 flex-1 min-w-full flex items-center container bg-purple-500 text-white">
+        <div className="sticky top-0 z-30 h-20 px-36 flex-1 min-w-full flex items-center container bg-purple-500 text-white justify-between">
             {renderDialog()}
-            <img src={`${process.env.PUBLIC_URL}/maplestory-icon.png`} alt="Icon" className="w-7 h-7"/>
-            <p className="text-xl">
-                Maplelist
+            <div className="flex">
+                <img src={`${process.env.PUBLIC_URL}/maplestory-icon.png`} alt="Icon" className="w-7 h-7" />
+                <p className="text-2xl">
+                    Maplelist
             </p>
-            <div onClick={() => { dispatch(setChecklistType(ChecklistType.dailyChecklist)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-lg ml-20 cursor-pointer">
-                Dailies
             </div>
-            <div onClick={() => { dispatch(setChecklistType(ChecklistType.weeklyBosses)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-lg ml-5 cursor-pointer">
-                Weeklies
+            <div className="flex justify">
+                <div onClick={() => { dispatch(setChecklistType(ChecklistType.dailyChecklist)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-xl ml-20 cursor-pointer">
+                    Dailies
+                </div>
+                <div onClick={() => { dispatch(setChecklistType(ChecklistType.weeklyBosses)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-xl ml-5 cursor-pointer">
+                    Weeklies
+                </div>
+                <div onClick={() => { dispatch(setChecklistType(ChecklistType.shiftChecklist)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-xl ml-5 cursor-pointer">
+                    Shift
+                </div>
+                <div onClick={() => {
+                    toggleDialog(DialogType.deleteStorage);
+                }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-xl ml-5 cursor-pointer">
+                    Clear Data
             </div>
-            <div onClick={() => { dispatch(setChecklistType(ChecklistType.shiftChecklist)) }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-lg ml-5 cursor-pointer">
-                Shift
-            </div>
-            <div onClick={()=>{
-                toggleDialog(DialogType.deleteStorage);
-            }} className="bg-purple-500 hover:bg-purple-700 text-white px-3 py-2 rounded-md text-lg ml-5 cursor-pointer">
-                Clear Character Data
             </div>
         </div>
     )
