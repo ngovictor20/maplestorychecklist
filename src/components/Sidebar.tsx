6d01b0bf98b6eb0ac7c8796483272834c132a6e2
Victor <ngo.victor20@gmail.com>
2021-03-06 18:16:52 -0500

    Made cards wider, styled button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CharacterList from 'components/Character/CharacterList'
 import useDialog from 'components/Dialog/useDialog';
 import { DialogType } from 'components/Dialog/types';
@@ -11,13 +11,13 @@ const SidebarContainer = styled.div`
 const Sidebar = () => {
     const { renderDialog, toggleDialog } = useDialog();
     return (
-        <SidebarContainer className="border-r-2 border-accent-grey bottom-0 w-full left-0 relative shadow-xl">
+        <SidebarContainer className="border-r-2 border-accent-grey bottom-0 w-full left-0 relative shadow-xl flex flex-col items-center text-header-grey">
             {renderDialog()}
             <div className="pt-6">
                 <CharacterList />
             </div>
-            <div onClick={() => { toggleDialog(DialogType.addCharacter) }} className="hover:bg-accent-grey hover:text-white absolute bottom-0 w-full h-14 text-center flex items-center justify-center">
-                    <p>Add Character</p>
+            <div onClick={() => { toggleDialog(DialogType.addCharacter) }} className="hover:bg-accent-grey hover:text-white absolute bottom-2 w-4/5 h-14 text-center flex items-center justify-center cursor-pointer p-2 border-2 border-accent-grey rounded-md">
+                <p>Add Character</p>
             </div>
         </SidebarContainer>
     )
